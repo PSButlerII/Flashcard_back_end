93c079cafc749276544b355397b073deeaec9884
PSButler <38290715+PSButlerII@users.noreply.github.com>
2021-07-28 17:07:25 -0400

    created new project. Completed models,views,and urls.  collections and cards path are both acccessable

---
+++ flashcardapp/flashcard_app/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Collections, Cards
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collections
+        fields = ['id', 'name', 'description']
+
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cards
+        fields = ['collection', 'question', 'answer', 'id']
